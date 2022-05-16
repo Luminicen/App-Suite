5e7986a9d4b611f47ab6332a3c85b89b90d8af63
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-05-16 12:22:02 -0300

    cambio de configuracion

--- App/app/app/settings.py
+++ App/app/app/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT="../static"
+STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATICFILES_DIRS = (os.path.join(
+    BASE_DIR, "app", "static"),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
