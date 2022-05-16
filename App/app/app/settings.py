9d913d93966474d1ba1c558de047c8230b5fca18
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-05-16 11:42:34 -0300

    agrego navBar y un poco de bootstrap a los forms

--- App/app/app/settings.py
+++ App/app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'suite.apps.SuiteConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
