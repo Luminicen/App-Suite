2c44c74febdf75a2de1c1821c8442429b2106d60
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-05-27 15:31:57 -0300

    funcionalidad de reemplazar string

--- App/app/app/static/scripts/reglasChequeo.js
+++ App/app/app/static/scripts/reglasChequeo.js
@@ -1,6 +1,5 @@
-window.onload = ()=>{
 var ep = document.getElementById('id_texto')
-console.log(ep.value)
+var id='id_texto'
 var Boundaries
 var contextMenu
 var texto=ep.value
@@ -9,6 +8,14 @@ var arr=[]
 var arrContext=[]
 var arrbonds=[]
 var arrOP=[] // voy a tener siempre una razon, lo que me varian son las opciones
+var arrInicio=[]
+var arrFin=[]
+var arrReemplazo=[]
+var largo = 0
+var clickActual
+window.onload = ()=>{
+
+
 axios({
     method: 'post',
     url: 'http://localhost:5000/passive_voice',
@@ -39,14 +46,15 @@ axios({
       //console.log(arrOP)
       //hightlighter
     
-      $('#id_texto').highlightWithinTextarea({
+      $('#'+id).highlightWithinTextarea({
         highlight: arr
     });
-    console.log("HOLA 3")
-    console.log(response.data[2]["Razon"])
+    //console.log("HOLA 3")
+    //console.log(response.data[2]["Razon"])
     //MENU CONTEXTUAL
+
     
-    let largo = 0
+    
     while (largo < (long+1)) {
     contextMenu = CtxMenu(document.querySelectorAll('.regla'+largo)[0]);
     //console.log(document.querySelectorAll('.regla'+largo)[0])
@@ -55,7 +63,8 @@ axios({
     p=1
     while(response.data[largo]["OP"+p]!= undefined){
       //console.log(response.data[largo]["OP"+p])
-      contextMenu.addItem( response.data[largo]["OP"+p][0]+" "+response.data[largo]["OP"+p][1], function(){console.log("HOLA")})
+      let reemplazo=response.data[largo]["OP"+p][1]
+      contextMenu.addItem( response.data[largo]["OP"+p][0]+" "+response.data[largo]["OP"+p][1],function(){reemplazarTexto(arrbonds,response,reemplazo) })
       p=p+1
     }
     //contextMenu.addItem( response.data[0]["OP1"][0]+" "+response.data[0]["OP1"][1], function(){console.log("HOLA")})
@@ -65,7 +74,7 @@ axios({
     Boundaries = document.querySelectorAll('.regla'+largo)[0].getBoundingClientRect();
     arrbonds.push(Boundaries)
     //console.log(arrbonds)
-    document.getElementById('id_texto').addEventListener('contextmenu', function(e) {
+    document.getElementById(''+id).addEventListener('contextmenu', function(e) {
       e.preventDefault()
       i=0
       while (i<(long+1)){ if (localizar(e,arrbonds,i)) {break} ;i=i+1}
@@ -83,23 +92,24 @@ axios({
     //contextMenu.addItem( response.data[0]["OP2"][0]+" "+response.data[0]["OP2"][1], function(){console.log("HOLA")})
 
     
-    document.getElementById('id_texto').addEventListener('click', function(e){e.preventDefault();
+    document.getElementById(''+id).addEventListener('click', function(e){e.preventDefault();
       q=0 
       while(q<=long){arrContext[q].closeMenu();q= q + 1}
       
       });
   
-  
+
 
 
   function localizar(e,arrbonds,largo){
-    var x = e.clientX;
+        var x = e.clientX;
         var y = e.clientY;
         var insideX = x >= arrbonds[largo].left && x <= arrbonds[largo].right;
         var insideY = y >= arrbonds[largo].top && y <= arrbonds[largo].bottom;
       
         if(insideX && insideY){
           console.log('On top of "one"!');
+          this.clickActual=[x,y]
           arrContext[largo].openMenu(x,y)
           return true
         }
@@ -109,4 +119,25 @@ axios({
         }
           
     }
+}
+function reemplazarTexto(arrbonds,response,text){
+ act=0
+ target=document.getElementById(id).value
+ while(arrbonds[act]!= undefined){
+  var insideX = clickActual[0] >= arrbonds[act].left && clickActual[0] <= arrbonds[act].right;
+  var insideY = clickActual[1] >= arrbonds[act].top && clickActual[1] <= arrbonds[act].bottom;
+   if(insideX && insideY){
+     break;
+   }
+   act=act+1
+ }
+ let ini
+ let fin
+ ini= response.data[act]["OP1"][2]
+ fin= response.data[act]["OP1"][3]
+var firstPart = target.substr(0,ini); // "this is "
+var lastPart = target.substr(fin);
+target=firstPart+text+lastPart
+console.log(target)
+document.getElementById(id).value=target
 }
