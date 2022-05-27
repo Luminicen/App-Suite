3d2dbda9777877867b0e9978c661130baca5c258
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-05-27 12:23:55 -0300

    refactoring + comentarios

--- App/app/app/static/scripts/reglasChequeo.js
+++ App/app/app/static/scripts/reglasChequeo.js
@@ -21,7 +21,7 @@ axios({
       long=response.data.length - 1
       let largoD= 0
       while (largoD < (long+1)){
-        //console.log(response.data.length)
+        //Realizo la carga de datos al arreglo que va a tener highlights
         arr.push({highlight: [response.data[largoD]["OP1"][2], response.data[largoD]["OP1"][3]],
         className: 'regla'+largoD})
         z=1
@@ -34,40 +34,45 @@ axios({
         
         largoD = largoD + 1
       }
-      //console.log(arr)
-      //console.log("ARREGLOS DE OP")
-      //console.log(arrOP)
-      //hightlighter
-    
-      $('#id_texto').highlightWithinTextarea({
+      //al elemento (TEXT AREA) le mando lo que hay que marcar
+      $ep.highlightWithinTextarea({
         highlight: arr
     });
-    console.log("HOLA 3")
-    console.log(response.data[2]["Razon"])
+    //console.log("HOLA 3")
+    //console.log(response.data[2]["Razon"])
+
     //MENU CONTEXTUAL
     
     let largo = 0
+    //le asigno al que esta marcado un menu de contexto y le agrego las opciones
     while (largo < (long+1)) {
     contextMenu = CtxMenu(document.querySelectorAll('.regla'+largo)[0]);
-    //console.log(document.querySelectorAll('.regla'+largo)[0])
+    //le inserto el primer item que es la razon por la que se marco
     contextMenu.addItem(response.data[largo]["Razon"])
     contextMenu.addSeparator(index = 1)
     p=1
+    //le inserto los reemplazos con la funcion de reemplazo correspondiente
     while(response.data[largo]["OP"+p]!= undefined){
-      //console.log(response.data[largo]["OP"+p])
       contextMenu.addItem( response.data[largo]["OP"+p][0]+" "+response.data[largo]["OP"+p][1], function(){console.log("HOLA")})
       p=p+1
     }
-    //contextMenu.addItem( response.data[0]["OP1"][0]+" "+response.data[0]["OP1"][1], function(){console.log("HOLA")})
-    //contextMenu.addItem( response.data[0]["OP2"][0]+" "+response.data[0]["OP2"][1], function(){console.log("HOLA")})
-    
+    //guardo el menu contextoal
+    //ACA COMIENZA EL LIO
+    //como tengo un problema de capas, porque al hacer click no le estoy haciendo click al elemento
+    //realmente marcado (que es el clon del text area con el elemento a marcar entre <MARK>) sino que
+    //le estoy haciendo click al text area comun (donde escribo)
+    //La SOLUCION: saco las coords x e y y si yo clickeo ahi es porque estoy clickeando al elemento
+    //marcado.
     arrContext.push(contextMenu)
+    //obtengo los bordes del highlighter y los guardo
     Boundaries = document.querySelectorAll('.regla'+largo)[0].getBoundingClientRect();
     arrbonds.push(Boundaries)
-    //console.log(arrbonds)
-    document.getElementById('id_texto').addEventListener('contextmenu', function(e) {
+    //por cada click detecho llamo a una funcion que previene el menu contextual clasico
+    //ademas de asignarle la funcion localizar para abrir el menu contextual
+    ep.addEventListener('contextmenu', function(e) {
       e.preventDefault()
       i=0
+      //si se abrio el menu rompe este while para seguir con la ejecucion normal
       while (i<(long+1)){ if (localizar(e,arrbonds,i)) {break} ;i=i+1}
       
     });
@@ -75,15 +80,8 @@ axios({
     }
 
   });
-    //contextMenu = CtxMenu(document.querySelectorAll('.regla')[0]);
-    //console.log(document.querySelectorAll('.regla0')[0])
-    //contextMenu.addItem(response.data[0]["Razon"])
-    //contextMenu.addSeparator(index = 1)
-    //contextMenu.addItem( response.data[0]["OP1"][0]+" "+response.data[0]["OP1"][1], function(){console.log("HOLA")})
-    //contextMenu.addItem( response.data[0]["OP2"][0]+" "+response.data[0]["OP2"][1], function(){console.log("HOLA")})
-
-    
-    document.getElementById('id_texto').addEventListener('click', function(e){e.preventDefault();
+    //un listener que cieerre todos los menus contextuales :D
+    ep.addEventListener('click', function(e){e.preventDefault();
       q=0 
       while(q<=long){arrContext[q].closeMenu();q= q + 1}
       
@@ -91,7 +89,8 @@ axios({
   
   
 
-
+//la funcion localizar se fija si el "click" esta dentro del elemento highlited
+//si lo esta abre el menu sino cierra y chau
   function localizar(e,arrbonds,largo){
     var x = e.clientX;
         var y = e.clientY;
