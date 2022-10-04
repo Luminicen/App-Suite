ac3294fc7ca6679966ed79f3e17364c28f1d35f8
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-10-04 14:28:09 -0300

    cambios para la expo

--- App/app/suite/views.py
+++ App/app/suite/views.py
@@ -45,8 +45,8 @@ def eliminarConcurrencia(user):
 #expoCiencia pass expo1234
 def proyectos(request):
     #obtengo todos los proyectos y filtro por los que son mios
-    #user = authenticate(request, username="expoCiencia", password="expo1234")
-    #login(request, user)
+    user = authenticate(request, username="expoCiencia", password="expo1234")
+    login(request, user)
     proyectos=Proyecto.objects.all()
     mio=[]
     ok=False
@@ -97,7 +97,7 @@ def modificarProyecto(request,id):
             proyecto.titulo=infForma['titulo']
             proyecto.participantes.set(infForma['participantes'])
             #desactivar para la expo
-            proyecto.save()   
+            #proyecto.save()   
             return redirect(reverse('proyectos')) 
     else:
         datosAModificar={'titulo': proyecto.titulo,'owner':proyecto.owner,'participantes':proyecto.participantes.all()}
@@ -232,7 +232,8 @@ def modificarArtefacto(request,id,idP):
             if puedeEscribirEnBd(request.user,id):
                 concu=Concurrencia.objects.get(nombre=request.user.username)
                 concu.delete()
-                artefacto.save()
+                #desactivar para expo
+                #artefacto.save()
                 return redirect(reverse('artefactos',kwargs={'id':idP}))
             else:
                 no_escribir=True
