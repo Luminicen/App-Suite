19db6769627150206167995aaf47962bdb9d8de5
Luminicen <70712333+Luminicen@users.noreply.github.com>
2022-10-24 12:56:08 -0300

    reglas actualizadas

--- App/app/app/static/scripts/alertasDeReglas.js
+++ App/app/app/static/scripts/alertasDeReglas.js
@@ -4,45 +4,11 @@ export default class AlertasDeReglas{
         this.campo = id
         this.tip=tipo
     }
-buscarLinea(texto,pos1,pos2){
-    let inicio_oracion = 0
-    let linea = 1
-    let punto = -1//donde termina la oracion
-    let i=0
-    while (i != texto.length){
-        if (texto[i] == ".") {
-            //encontre el punto
-            punto = i
-            //pregunto si la posicion esta antes del punto y despues del  inicio de oracion
-            if ((pos1 >= inicio_oracion) && (pos2 <= punto)){
-                return linea
-            }
-            else {
-                linea = linea + 1
-                inicio_oracion = punto + 1
-                punto = -1
-            }
-        }
-        i= i + 1
-    }
-    //si no encontro el punto devuelvo la linea igual. en el peor de los casos es linea 1 no enchufo un punto
-    return linea
-}
-devolverLineasAfectadas(res){
-    let desc=""
-    let x = 0
-    let lista = new Set()
-    while (res.data[x]!=undefined){
-        lista.add(this.buscarLinea(document.getElementById(this.campo).value,res.data[x]["OP1"][2],res.data[x]["OP1"][3]))
-        x= x + 1
-    }
-    for (let item of lista) desc = desc + item+" "
-    return desc
-}
+
 ejecutar(){
 
 
-axios.post('http://apirequesem-requirements-healer.okd.lifia.info.unlp.edu.ar/one_verb', 
+axios.post('http://127.0.0.1:5000/api', 
 {
     data:document.getElementById(this.campo).value,
 }
@@ -51,77 +17,37 @@ axios.post('http://apirequesem-requirements-healer.okd.lifia.info.unlp.edu.ar/on
 res=>{ 
 
     const element = document.getElementById("advertencias");
-    if (res.data.length !=0){
+    if (res.data["has_more_verb"].length !=0){
+        console.log(res.data)
+        let para = document.createElement("h5");
+        let node = document.createTextNode("Excess of verbs in the field "+this.campo.slice(3,-1)+", in the sentence/s "+obtenerOraciones("has_more_verb",res)+". It is recommended to split the sentence");
+        para.appendChild(node);
+        element.appendChild(para);
+    }
+    if (res.data["null_subject"].length !=0){ 
+        let para = document.createElement("h5");
+        let node = document.createTextNode("There is no subject in  : "+this.campo.slice(3,-1)+" in the sentence/s: "+obtenerOraciones("null_subject",res));
+        para.appendChild(node);
+        element.appendChild(para);
+    }
+    if (res.data["hasAdjOrAdv"].length !=0){
+        let para = document.createElement("h5");
+        let node = document.createTextNode("There is a sentence with adjectives or adverbs in the field "+this.campo.slice(3,-1)+", in the sentence/s "+obtenerOraciones("hasAdjOrAdv",res)+". Please consider replace the adjectives and adverbs with new sentences describing them");
+        para.appendChild(node);
+        element.appendChild(para);
+    }
+    if (res.data["has_passive_voice"].length !=0){
+        
         
         let para = document.createElement("h5");
-        let node = document.createTextNode("Excess of verbs in the field "+this.campo.slice(3,-1)+", in line/s "+this.devolverLineasAfectadas(res)+". It is recommended to split the sentence");
+        let node = document.createTextNode("There is a sentence with passive voice "+this.campo.slice(3,-1)+" in line/s "+obtenerOraciones("has_passive_voice",res));
         para.appendChild(node);
         element.appendChild(para);
     }
 
-})
-
-axios.post('http://apirequesem-requirements-healer.okd.lifia.info.unlp.edu.ar/null_subject', 
-{
-    data:document.getElementById(this.campo).value,
-}
-).then(
-
-res=>{ 
-
-    const element = document.getElementById("advertencias");
-    if (res.data.length !=0){
-        
-        
-        let para = document.createElement("h5");
-        let node = document.createTextNode("There is no subject in  : "+this.campo.slice(3,-1)+" in line/s :"+this.devolverLineasAfectadas(res));
-        para.appendChild(node);
-        element.appendChild(para);
-    }
 
 })
 
-axios.post('http://apirequesem-requirements-healer.okd.lifia.info.unlp.edu.ar/adj_and_adv', 
-{
-    data:document.getElementById(this.campo).value,
-}
-).then(
-
-res=>{ 
-
-    const element = document.getElementById("advertencias");
-    if (res.data.length !=0){
-        
-        
-        let para = document.createElement("h5");
-        let node = document.createTextNode("There is a sentence with adjectives or adverbs in the field "+this.campo.slice(3,-1)+", in line/s "+this.devolverLineasAfectadas(res)+". Please consider replace the adjectives and adverbs with new sentences describing them");
-        para.appendChild(node);
-        element.appendChild(para);
-    }
-
-    
-})
-
-axios.post('http://apirequesem-requirements-healer.okd.lifia.info.unlp.edu.ar/passive_voice', 
-{
-    data:document.getElementById(this.campo).value,
-}
-).then(
-
-res=>{ 
-
-    const element = document.getElementById("advertencias");
-    if (res.data.length !=0){
-        
-        
-        let para = document.createElement("h5");
-        let node = document.createTextNode("There is a sentence with passive voice "+this.campo.slice(3,-1)+" in line/s "+this.devolverLineasAfectadas(res));
-        para.appendChild(node);
-        element.appendChild(para);
-    }
-
-    
-})
 
 }
 ejecutarLimpieza(){
@@ -134,4 +60,14 @@ ejecutarLimpieza(){
     
     }
 
+}
+function obtenerOraciones(tipo,res){
+        let z=0
+        let lineas=""
+        while(res.data[tipo][z]!= undefined){
+            
+            lineas= lineas +res.data[tipo][z]["Oracion"]+" "
+            z = z + 1
+        }
+        return lineas  
 }
