0c0fedcdc8ed4038963d3fe9fad07e71e0665edb
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-04-19 14:19:22 -0300

    mas cuestiones de init

--- Services/IA/moduloIa/settings.py
+++ Services/IA/moduloIa/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'aplicacionIA',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,16 @@ WSGI_APPLICATION = 'moduloIa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'req',
+        'USER': 'root',
+        'PASSWORD': '', # poner password para produccion
+        'HOST': 'localhost',   # para produccion dejar db
+        'PORT': '3306',
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
