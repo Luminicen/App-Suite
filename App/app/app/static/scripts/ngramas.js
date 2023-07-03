771f36b609da9cf7dd72371648bf6980fc73121c
Luminicen <70712333+Luminicen@users.noreply.github.com>
2023-07-03 13:54:01 -0300

    bosquejo de agregar opciones

---
+++ App/app/app/static/scripts/ngramas.js
@@ -0,0 +1,7 @@
+export function agregarData(label){
+    console.log(label)
+    console.log("HOLA MUNDO")
+    var datos = JSON.parse(document.getElementById('ngram').textContent);
+    console.log(datos)
+    let i = 0;
+}
