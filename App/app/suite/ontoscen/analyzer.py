fe892db5886ea5bcdb2254efcaeaa6165997fc7f
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-02-24 10:11:12 -0300

    .

--- App/app/suite/ontoscen/analyzer.py
+++ App/app/suite/ontoscen/analyzer.py
@@ -5,6 +5,7 @@ from rdflib import URIRef
 from collections import Counter
 
 from .helpers import get_user_input
+import spacy
 
 NLP = spacy.load("en_core_web_sm")
 
@@ -310,20 +311,22 @@ class Analyzer:
         self._remove_rules_for_resources()
         return result + included_resources
 
-    def get_sentence_without_actors(self, sentence):
-        actor = self.analyze_for_actors(sentence)
-        print("sentence", sentence)
-        print("actor", actor)
-        return sentence.text.replace(actor, "").strip()
+    def get_infinitive_sentence(self, sentence):
+        lemmas = []
+        for token in NLP(sentence):
+            if token.dep_ == "nsubj":
+                continue
+            if token.pos_ == "VERB":
+                lemmas.append(token.lemma_)
+            elif token.pos_ != "DET" and token.pos_ != "PUNCT":
+                lemmas.append(token.text)
+        return " ".join(lemmas)
     
     def are_sentences_equivalent(self, sentence1, sentence2):
-        sentence1 = NLP(self.get_sentence_without_actors(sentence1))
-        # print("sentence1", sentence1)
-        sentence1_lemmatized = self.lemmatize(sentence1)
-        # print("sentence1_lemmatized", sentence1_lemmatized)
-        sentence2 = NLP(self.get_sentence_without_actors(sentence2))
-        # print("sentence2", sentence2)
-        sentence2_lemmatized = self.lemmatize(sentence2)
-        # print("sentence2_lemmatized", sentence2_lemmatized)
-        # print("similarity", sentence1.similarity(sentence2))
+        sentence1 = NLP(self.get_infinitive_sentence(sentence1))
+        sentence2 = NLP(self.get_infinitive_sentence(sentence2))
+        print("sentence1", sentence1)
+        print("sentence2", sentence2)
+        print("similarity", sentence1.similarity(sentence2))
+        print("\n\n")
         return sentence1.similarity(sentence2) > 0.9
